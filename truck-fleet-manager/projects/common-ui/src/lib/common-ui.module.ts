fad636756d26f85956441cc009b99436c3511996
gturconi <35380017+gturconi@users.noreply.github.com>
2023-10-25 11:44:58 -0300

    Search Bar added

--- truck-fleet-manager/projects/common-ui/src/lib/common-ui.module.ts
+++ truck-fleet-manager/projects/common-ui/src/lib/common-ui.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -24,6 +24,7 @@ import { FormComponent } from './form/form.component';
     MatCardModule,
     MatTableModule,
     MatIconModule,
+    FormsModule
   ],
   exports: [
     FormComponent,
--- truck-fleet-manager/projects/common-ui/src/lib/table/table.component.ts
+++ truck-fleet-manager/projects/common-ui/src/lib/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input,Output } from '@angular/core';
+import { Component, EventEmitter, Input,OnChanges,Output, SimpleChanges } from '@angular/core';
 import { DELETE_CONFIRMATION_MESSAGE } from '../messages.constant';
 
 @Component({
@@ -7,16 +7,24 @@ import { DELETE_CONFIRMATION_MESSAGE } from '../messages.constant';
   styleUrls: ['./table.component.scss']
 })
 
-export class TableComponent {
+export class TableComponent implements OnChanges {
  
-  @Input() data: any[] = [];
+  @Input() data: any[] = [];  
   @Input() columns: string[] = [];
 
+  filteredData: any[] = [];
+  search: string = '';
   displayedColumns: string[] = [];
 
   ngOnInit(): void {
     this.displayedColumns = [...this.columns, 'actions'];
   }  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data'] && changes['data'].currentValue) {      
+      this.filteredData = changes['data'].currentValue;
+    }
+  }
 
   @Output() edit = new EventEmitter<any>();
   @Output() delete = new EventEmitter<any>();
@@ -31,4 +39,11 @@ export class TableComponent {
       this.delete.emit(item);
     }    
   }
+
+  filterData() {
+    this.filteredData = this.data.filter((row) => {
+      return row.patent.toLowerCase().includes(this.search.toLowerCase());
+    });
+  }
+  
 }
