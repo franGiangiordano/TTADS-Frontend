1c2c0e17e9fd648938006926ca543cf8809e9cc0
Martin Oscar Lambrecht <martinoscarlambrecht@gmail.com>
2023-11-01 20:59:08 -0300

    Extract guards to common lib

---
+++ truck-fleet-manager/projects/common/src/guards/operative.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AppLoginService } from 'projects/app-login/src/lib/services/app-login.service';
+import { UserRoles } from 'projects/app-login/src/lib/models/user.roles.enum';
+
+export const operativeGuard: CanActivateFn = (route, state) => {
+  const loginService = inject(AppLoginService)
+  const router = inject(Router);
+  let roles: UserRoles[] = loginService.getUserRole();
+
+  if (loginService.isLoggedIn() && roles.some(role => role === UserRoles.Operative)) {
+    return true; 
+  } else {
+    router.navigate(['login']);
+    return false; 
+  }
+};
