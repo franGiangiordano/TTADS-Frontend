f845bbd6634551f99a27fea3f2c28244ab7f36e0
gturconi <35380017+gturconi@users.noreply.github.com>
2023-11-07 15:43:25 -0300

    Update app-login.routing.module.ts

--- truck-fleet-manager/projects/app-login/src/lib/app-login.routing.module.ts
+++ truck-fleet-manager/projects/app-login/src/lib/app-login.routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginHomeComponent } from "./login-home/login-home.component";
-import { adminGuard } from "projects/common/src";
-
 
 const routes: Routes = [
     { path: '', component: LoginHomeComponent }
