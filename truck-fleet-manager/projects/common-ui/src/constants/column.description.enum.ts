c6d712de2b18576b27b783d63b69298ec27ed69a
gturconi <35380017+gturconi@users.noreply.github.com>
2023-11-11 16:50:17 -0300

    ASD

---
+++ truck-fleet-manager/projects/common-ui/src/constants/column.description.enum.ts
@@ -0,0 +1,11 @@
+export enum ColumnDescription {
+    patent = 'Patente',
+    name = 'Nombre',
+    surname = 'Apellido',
+    legajo = 'Legajo',
+    type = 'Tipo',
+    description = 'Descripcion',
+    until_date = 'Fecha Hasta',
+    batea = 'Patente Batea',
+    trailer = 'Patente Trailer'
+} 
