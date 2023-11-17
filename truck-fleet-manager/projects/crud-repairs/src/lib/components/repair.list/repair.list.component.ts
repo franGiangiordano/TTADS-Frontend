9c093d52fae4f3c0c85d6496da93500c6e1d706b
franGiangiordano <frangiangiordano@gmail.com>
2023-11-17 15:09:25 -0300

    Equipment added to repair table

--- truck-fleet-manager/projects/crud-repairs/src/lib/components/repair.list/repair.list.component.ts
+++ truck-fleet-manager/projects/crud-repairs/src/lib/components/repair.list/repair.list.component.ts
@@ -52,10 +52,10 @@ export class RepairlistComponent implements OnInit {
     return array.map(obj => {
       return {
         _id: obj._id,
+        descEquipo: obj.equipment.description,   
         legajo: obj.equipment.driver.legajo,
         name: obj.equipment.driver.name,
-        surname: obj.equipment.driver.surname,
-        description: obj.equipment.description,        
+        surname: obj.equipment.driver.surname,   
         batea: obj.equipment.batea.patent,
         trailer: obj.equipment.trailer.patent,
         reparacion: obj.description,
