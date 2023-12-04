532f9e82770f8199f127bf1d21118769d8793218
gturconi <35380017+gturconi@users.noreply.github.com>
2023-12-04 10:51:55 -0300

    Code refactoring && additional cards added

--- truck-fleet-manager/projects/dashboard/dashboard-component/dashboard.component.ts
+++ truck-fleet-manager/projects/dashboard/dashboard-component/dashboard.component.ts
@@ -1,27 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { forkJoin } from 'rxjs';
+
 import { DriverService } from '../../../projects/crud-drivers/src/lib/services/driver.service';
 import { Driver } from '../../../projects/crud-drivers/src/lib/models/driver.model';
 import { EntityListResponse } from 'projects/common/src';
+import { BateaService } from 'projects/crud-bateas/src/lib/services/batea.service';
+import { TrailerService } from 'projects/crud-trailers/src/lib/services/trailer.service';
+import { TravelService } from 'projects/crud-travels/src/lib/services/travel.service';
+import { EquipmentService } from 'projects/crud-equipment/src/lib/services/equipment.service';
+import { RepairService } from 'projects/crud-repairs/src/lib/services/repair.service';
 
 @Component({
   selector: 'lib-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  totalDriversCount: number = 0;
+export class DashboardComponent implements OnInit {  
+  cardInfo!: any[];
 
-  constructor(private driverService: DriverService) { }
+  driverService = inject(DriverService);
+  bateaService = inject(BateaService);
+  trailerService = inject(TrailerService);
+  travelService = inject(TravelService);
+  equipmentService = inject(EquipmentService);
+  repairService = inject(RepairService);
 
   ngOnInit(): void {
-    this.countDrivers();
+    this.getCounts();
+    this.cardInfo = [
+      {
+        title: 'Choferes',
+        icon: 'fa-id-card'
+      },
+      {
+        title: 'Bateas',
+        icon: 'fa-truck'
+      },
+      {
+        title: 'Trailers',
+        icon: 'fa-truck'
+      },
+      {
+        title: 'Viajes',
+        icon: 'fa-route'
+      },
+      {
+        title: 'Reparaciones',
+        icon: 'fa-gears'
+      },
+      {
+        title: 'Equipos',
+        icon: 'fa-users-gear'
+      }
+    ]
   }
 
-  countDrivers(): void {
-    this.driverService.getDrivers() 
-      .subscribe((response: EntityListResponse<Driver>) => {
-        this.totalDriversCount = response.results.length;
-      });
+  getCounts(): void {
+    forkJoin([
+      this.driverService.getDrivers(),
+      this.bateaService.getBateas(),
+      this.trailerService.getTrailers(),
+      this.travelService.getTravels(),
+      this.repairService.getRepairs(),
+      this.equipmentService.getEquipments()
+    ]).subscribe((responses: any) => {     
+      console.log(responses);
+      for (let i = 0; i < responses.length; i++) {
+        this.cardInfo[i].count = responses[i].count;
+      }      
+    });
   }
 }
 
