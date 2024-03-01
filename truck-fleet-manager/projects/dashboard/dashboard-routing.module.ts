976dc4424b4cddfa114943a835e404cfad9a8f7e
gturconi <35380017+gturconi@users.noreply.github.com>
2024-02-29 22:09:27 -0300

    Update dashboard-routing.module.ts

--- truck-fleet-manager/projects/dashboard/dashboard-routing.module.ts
+++ truck-fleet-manager/projects/dashboard/dashboard-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard-component/dashboard.component';
+import { adminGuard } from 'projects/common/src';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-  }
+    canActivate: [adminGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
